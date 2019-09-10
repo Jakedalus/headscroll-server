86eaf3de7786567d2f7c3bfcfda46b100bc84bcc
Jakedalus <jacquedalus@gmail.com>
2019-09-10 13:46:23 -0400

    Add errorHandler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const errorHandler = require('./handlers/error');
 const authRoutes = require('./routes/auth');
 
 const PORT = process.env.PORT || 8081;
@@ -12,6 +13,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
+
+
+app.use(errorHandler);
 
 app.listen(PORT, function() {
   console.log(`Server is running on port ${PORT}`);
