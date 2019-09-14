b3229ec782daa60a046d66a54aa9774fd1c5b4c5
Jakedalus <jacquedalus@gmail.com>
2019-09-14 14:04:37 -0400

    Add delete friend/request route

--- routes/friends.js
+++ routes/friends.js
@@ -92,4 +92,57 @@ router.route('/').post(async function(req, res, next) {
   }
 });
 
+router.route('/').delete(async function(req, res, next) {
+  try {
+    console.log('params:', req.params);
+
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, async function(err, decoded) {
+      if (decoded) {
+        let requestingUser = await db.User.findById(decoded.id);
+        let requestedUser = await db.User.findById(req.params.id);
+
+        const pickedRequestedUser = (({ _id }) => ({ _id }))(requestedUser);
+        const pickedRequestingUser = (({ _id }) => ({ _id }))(requestingUser);
+
+        console.log('users route, POST, user, _id:', pickedRequestedUser, pickedRequestingUser);
+
+        // if the other user has already requested you as a friend
+        if (requestingUser.requests.includes(pickedRequestedUser._id)) {
+          // delete, i.e. Decline, the request
+          requestingUser.requests.splice(requestingUser.requests.indexOf(pickedRequestedUser._id), 1);
+          await requestingUser.save(); 
+        // if you have requested their friendship already
+        } else if (requestedUser.requests.includes(pickedRequestingUser._id)){
+          // delete, i.e. Cancel, friend request
+          requestedUser.requests.splice(requestedUser.requests.indexOf(pickedRequestingUser._id), 1);
+          await requestedUser.save(); 
+        // if you are already friends with the user
+        } else if (requestedUser.friends.includes(pickedRequestingUser._id)){
+          // remove yourself from their friends list
+          requestedUser.friends.splice(requestedUser.friends.indexOf(pickedRequestingUser._id), 1);
+          // remove them from their friends list
+          requestingUser.friends.splice(requestingUser.friends.indexOf(pickedRequestedUser._id), 1);
+          await requestedUser.save(); 
+          await requestingUser.save(); 
+        }
+
+        
+
+        return res.status(200).json(pickedRequestedUser);
+        
+      } else {
+        return next({
+          status: 401,
+          message: 'Please log in first'
+        });
+      }
+    });
+    
+
+  } catch (err) {
+    return next(err);
+  }
+});
+
 module.exports = router;
