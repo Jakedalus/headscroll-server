e36243148646fdba9a1539b27ff2ac54a1f4a63a
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-09-11 15:28:44 -0400

    Change database env variable to migrate to mongodb Atlas since mlab is shutting down

--- models/index.js
+++ models/index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/headscroll', {
-  keepAlive: true
-});
+mongoose.connect(
+	process.env.DB_URI || 'mongodb://localhost/headscroll',
+	{
+		keepAlive : true
+	}
+);
 
 module.exports.User = require('./user');
 module.exports.Post = require('./post');
